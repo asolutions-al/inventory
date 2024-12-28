f2399f223538eb6457f78baa1251a231c159036f
anxhi <anxhirr@gmail.com>
2024-12-28 13:28:32 +0100

    feaT: bumb next intl version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const createNextIntlPlugin = require("next-intl/plugin")
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin({
+  requestConfig: "./i18n.ts",
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
